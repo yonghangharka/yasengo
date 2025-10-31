35cdd3e466d69d3116e8696c1ad3496db85a041d
Harkahang Yonghang <harkahangyonghang@macbook.local>
2025-10-31 14:43:14 +0545

    message

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <p className="text-xl">
+        This is just demo page which im working on and and will update some
+        times if possible.
+      </p>
+    </div>
+  );
+}
